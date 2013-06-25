b82a861e4c20af03f1ccad1805f0a9b04848683e
Michael Freedman <github@michael-freedman.com>
2013-06-25 19:43:22 -0400

    create Entry

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20130625232049) do
+
+  create_table "entries", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
