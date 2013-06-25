b0a1ac00ad61c6e2c76564c56ab68effb6192881
Michael Freedman <github@michael-freedman.com>
2013-06-25 16:50:32 -0400

    boom

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Glossarium::Application.config.secret_key_base = '00976d8d973a382ae888e9dddc878599ddf9481fa373b5836e50ac274029172289112ed4134df39d5dd2431f20c2be8bf8391908b9edef8ac685ebcdd181ee2c'
